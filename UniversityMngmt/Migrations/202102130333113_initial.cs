158075d4f2ea3183db7a8bfb80ffda867ff1296b
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-02-12 19:36:37 -0800

    Created Student Acct database to project

---
+++ UniversityMngmt/Migrations/202102130333113_initial.cs
@@ -0,0 +1,28 @@
+﻿namespace UniversityMngmt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.StudentAccounts",
+                c => new
+                    {
+                        AccountId = c.Int(nullable: false, identity: true),
+                        AccountName = c.String(nullable: false),
+                        AccountDescription = c.String(),
+                        AccountBalance = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.AccountId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.StudentAccounts");
+        }
+    }
+}
