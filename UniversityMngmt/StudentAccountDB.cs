37066d3c3cf225fe846bf69fc6f0a1397fe21c68
doms92 <summers8734@student.cptc.edu>
2021-02-13 17:36:54 -0800

    Created Student Account DB

---
+++ UniversityMngmt/StudentAccountDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityMngmt
+{
+    class StudentAccountDB
+    {
+        public static List<StudentAccount>
+        using (var context = new StudentContext())
+    {
+        List<StudentAccount> acc = (from fa in StudentAccount
+                                    orderby fa.AccountId ascending
+                                    select fa).ToList();
+
+        }
+    }
+
