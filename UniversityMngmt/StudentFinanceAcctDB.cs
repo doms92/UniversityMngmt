38d3c9ece679cd19501a8254a6f03066177ebb5d
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-02-14 17:57:42 -0800

    Added update functionality to StudentFinanceAcct

--- UniversityMngmt/StudentFinanceAcctDB.cs
+++ UniversityMngmt/StudentFinanceAcctDB.cs
@@ -1,51 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UniversityMngmt
 {
-
-   public static class StudentFinanceAcctDB
+    public static class StudentFinanceAcctDB
     {
         /// <summary>
         /// Returns a list of all financial accounts,
         /// sorted by AccountId, in ascending order.
         /// </summary>
-        public static List<StudentAccount>
-        using (var context = new StudentContext())
-    {
-        List<StudentFinanceAcct> acc = (from fa in StudentAccount
-                                    orderby fa.AccountId ascending
-                                    select fa).ToList();
+        public static List<StudentFinanceAcct> GetAllStudents() 
+        {
+            throw new NotImplementedException();
+        }
 
-        return acc;
+        /// <summary>
+        /// Adds a Student's financial account to the DB
+        /// </summary>
+        public static StudentFinanceAcct Add(StudentFinanceAcct acct)
+        {
+            using (var context = new StudentContext())
+            {
+                context.StudentFinanceAccts.Add(acct);
+
+                context.SaveChanges();
+
+                return acct;
+            }
+        }
+
+        /// <summary>
+        /// Updates/Edits all financial account data on student's account
+        /// </summary>
+        public static StudentFinanceAcct Update(StudentFinanceAcct acct)
+        {
+            using(var context = new StudentContext()) 
+            {
+                context.Entry(acct).State = EntityState.Modified;
+
+                context.SaveChanges();
+
+                return acct;
+            }
+        }
+
+        /// <summary>
+        /// Deletes a financial account from the DB
+        /// </summary>
+        public static void Delete(StudentFinanceAcct acct)
+        {
+            throw new NotImplementedException();
         }
     }
-/// <summary>
-/// Adds A Student Financial account to the DB
-/// </summary>
-
-public static StudentFinanceAcct Add(StudentFinanceAcct acct)
-{
-    using (var context = new StudentContext())
-    {
-        context.StudentFinanceAcct.Add(acct);
-        context.SaveChanges();
-    }
-    return acct;
 }
-
-/// <summary>
-/// Updates/Edits all Financial Account data
-/// </summary>
-public static StudentFinanceAcct Update(StudentFinanceAcct acct)
-{
-    
-}
- /// <summary>
- /// Deletes a financial account from the DB
- /// </summary>
-public static void Delete(StudentFinanceAcct)
-
