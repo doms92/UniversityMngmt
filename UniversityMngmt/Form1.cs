22b7bd529ccba9c69679b45bacf1a849f12ee486
doms92 <summers8734@student.cptc.edu>
2021-03-14 10:57:08 -0700

    Changed Form name to FinAccManager

--- UniversityMngmt/Form1.cs
+++ UniversityMngmt/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace UniversityMngmt
 {
-    public partial class Form1 : Form
+    public partial class FormFinAccManager : Form
     {
-        public Form1()
+        public FormFinAccManager()
         {
             InitializeComponent();
         }
