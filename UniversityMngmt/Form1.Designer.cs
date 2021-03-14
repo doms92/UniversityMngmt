baa50f3104e205e55c27bc5447ecf72cf0c3484b
doms92 <summers8734@student.cptc.edu>
2021-03-14 11:11:47 -0700

    Added Update / Edit Button to Accounts Manager Form

--- UniversityMngmt/Form1.Designer.cs
+++ UniversityMngmt/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdateEdit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnDelete
@@ -42,11 +43,22 @@
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnUpdateEdit
+            // 
+            this.btnUpdateEdit.Location = new System.Drawing.Point(72, 509);
+            this.btnUpdateEdit.Margin = new System.Windows.Forms.Padding(5);
+            this.btnUpdateEdit.Name = "btnUpdateEdit";
+            this.btnUpdateEdit.Size = new System.Drawing.Size(267, 93);
+            this.btnUpdateEdit.TabIndex = 1;
+            this.btnUpdateEdit.Text = "Update / Edit";
+            this.btnUpdateEdit.UseVisualStyleBackColor = true;
+            // 
             // FormFinAccManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 850);
+            this.Controls.Add(this.btnUpdateEdit);
             this.Controls.Add(this.btnDelete);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormFinAccManager";
@@ -59,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdateEdit;
     }
 }
 
